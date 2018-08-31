c9b0f6ae5dc98e5e15019a946cf4027cd3c1df75
y93x08y05 <1064807182@qq.com>
2018-08-31 16:37:09 +0800

    add some change

--- src/main/java/com/jack/demo/MapDemo.java
+++ src/main/java/com/jack/demo/MapDemo.java
@@ -13,6 +13,46 @@ public class MapDemo {
     private static int [][]arr={{5,2},{5,6},{5,11},{5,15},{5,19}, {5,23},
             {11,2},{11,6},{11,11},{11,15},{11,19}, {11,23}};
     public static void main(String[] args) {
+        String requestBody = "{\n" +
+                "  \"Inputs\": {\n" +
+                "    \"input1\": {\n" +
+                "      \"ColumnNames\": [\n" +
+                "        \"A\",\n" +
+                "        \"B\",\n" +
+                "        \"C\",\n" +
+                "        \"D\",\n" +
+                "        \"E\",\n" +
+                "        \"F\",\n" +
+                "        \"G\",\n" +
+                "        \"H\",\n" +
+                "      ],\n" +
+                "      \"Values\": [\n" +
+                "        [\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "        ],\n" +
+                "        [\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "          \"-90\",\n" +
+                "        ]\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"GlobalParameters\": {}\n" +
+                "}";
+        System.out.println(requestBody);
         createData(arr);
         System.out.println("X-axis is "+getData(12).get(0)+"\nY-axis is "+getData(12).get(1));
     }
@@ -25,6 +65,10 @@ public class MapDemo {
         }
     }
     private static List<Integer> getData(int key) {
+        int A=3;
+        int B=4;
+        String a1="\""+A+"\""+",\n"+"\""+B+"\""+",\n";
+        System.out.println(a1);
         return map.get(key);
     }
 }
