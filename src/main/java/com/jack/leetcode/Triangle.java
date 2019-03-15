a9894830178b5de112d93a6f28a773699e0dfd64
y93x08y05 <1064807182@qq.com>
2019-03-15 14:48:52 +0800

    add some leetcode

--- src/main/java/com/jack/leetcode/Triangle.java
+++ src/main/java/com/jack/leetcode/Triangle.java
@@ -7,7 +7,6 @@ import java.util.List;
  * @Author: Jack
  * @Date: 2019/3/15 13:35
  * LeetCode 120
- * 动态规划问题求解
  * 给定一个三角形，找出自顶向下的最小路径和。
  * 每一步只能移动到下一行中相邻的结点上。
  * [
@@ -17,6 +16,7 @@ import java.util.List;
  *   [4,1,8,3]
  * ]
  * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * 动态规划问题求解
  */
 public class Triangle {
     public static List<List<Integer>> lists = new ArrayList<>();
